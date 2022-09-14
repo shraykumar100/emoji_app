d4bb708e95e757c6a8d8297292a46c129b9b3cac
shrey <shrey100@gmail.com>
2022-09-14 23:26:22 +0530

    deployement fixed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ const Nav = () => {
   return (
     <nav>
       <h2>Emojee</h2>
-      <a href="https://github.com/shraykumar100/emoji_app" target="_blank">Github</a>
+      <a href="https://github.com/shraykumar100/emoji_app" target="_blank" rel="noreferrer">Github</a>
     </nav>
   )
 }
